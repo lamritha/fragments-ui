71f3d0dc521aa02b0062b932f5e1020d2a833f78
Amritha Lingeswaran <amriling06@gmail.com>
2026-05-15 22:19:16 -0400

    resolved lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
+
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]);
